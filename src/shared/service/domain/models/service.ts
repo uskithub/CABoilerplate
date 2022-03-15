441847d0af1fbf1840bcdd47386e195118836e8e
Yusuke SAITO <yusuke.saito@jibunstyle.com>
2022-03-15 22:39:13 +0900

    Usecaseのtype guardを追加

--- src/shared/service/domain/models/service.ts
+++ src/shared/service/domain/models/service.ts
@@ -1,6 +1,7 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import dependencies from "../dependencies";
 import { Usecase } from "@/shared/system/interfaces/usecase";
-import { BootUsecase } from "../../application/usecases/boot";
+import { Boot, BootUsecase } from "../../application/usecases/boot";
 import { SignInUsecase } from "../../application/usecases/signIn";
 import { SignOutUsecase } from "../../application/usecases/signOut";
 import { SignUpUsecase } from "../../application/usecases/signUp";
@@ -14,8 +15,9 @@ export default {
         return dependencies.auth.signInStatus();
     }
 
-    , authorize: <T, U extends Usecase<T>>(actor: User|null, initialScene: U): boolean => {
-        switch (initialScene.constructor) {
+    , authorize: <T, U extends Usecase<T>>(actor: User|null, usecase: U): boolean => {
+
+        switch (usecase.constructor) {
         case BootUsecase: {
             return true;
         }
@@ -28,6 +30,24 @@ export default {
         case SignOutUsecase: {
             return false;
         }
+        }
+
+        const isBoot = (usecase: any): usecase is BootUsecase => usecase.constructor === BootUsecase;
+        const isSignUp = (usecase: any): usecase is SignUpUsecase => usecase.constructor === SignUpUsecase;
+        const isSignIn = (usecase: any): usecase is SignInUsecase => usecase.constructor === SignInUsecase;
+        const isSignOut = (usecase: any): usecase is SignOutUsecase => usecase.constructor === SignOutUsecase;
+
+        if (isBoot(usecase)) {
+            if (usecase.context.scene === Boot.userOpensSite) {
+                // TODO
+            }
+            return true;
+        } else if (isSignUp(usecase)) {
+            return true;
+        } else if (isSignIn(usecase)) {
+            return true;
+        } else if (isSignOut(usecase)) {
+            return true;
         }
         return false;
     }
