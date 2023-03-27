3f406de912335cc24f5c569343e37d1d7ba81357
Yusuke SAITO <yusuke.saito@jibunstyle.com>
2023-03-27 23:26:26 +0900

    VNavigationDrawerがなぜかfontawesomeが必要というので依存関係を追加（なぜかmdiではできない）

--- src/client/system/plugins/vuetify.ts
+++ src/client/system/plugins/vuetify.ts
@@ -6,17 +6,20 @@ import "vuetify/styles";
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
-import { mdi } from "vuetify/iconsets/mdi";
-// import { aliases, fa } from "vuetify/iconsets/fa-svg";
-// import { library } from "@fortawesome/fontawesome-svg-core";
-// import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-// import { far } from "@fortawesome/free-regular-svg-icons";
+import { aliases, mdi } from "vuetify/iconsets/mdi-svg"
+import { fa } from "vuetify/iconsets/fa-svg"
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
+import { far } from "@fortawesome/free-regular-svg-icons";
+// import { fas } from "@fortawesome/free-solid-svg-icons"
 
 import { App } from "vue";
 
 export function loadVuetify(app: App<Element>) {
-  // app.component("font-awesome-icon", FontAwesomeIcon); // Register component globally
-  // library.add(far); // Include needed icons
+
+  app.component('font-awesome-icon', FontAwesomeIcon) // Register component globally
+  library.add(far) // Include needed icons
+  // library.add(fas) // Include needed icons
 
   const vuetify = createVuetify(
     // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
@@ -25,10 +28,10 @@ export function loadVuetify(app: App<Element>) {
       directives,
       icons: {
         defaultSet: "mdi",
-        // aliases,
+        aliases,
         sets: {
           mdi,
-          // fa,
+          fa,
         }
       },
     }
