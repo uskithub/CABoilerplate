3d27b805e5cec1e73a92bf361b87c38b5ffb7d48
Yusuke SAITO <yusuke.saito@jibunstyle.com>
2023-11-25 15:10:53 +0900

    再設計版に追随

--- src/shared/service/application/usecases/index.ts
+++ src/shared/service/application/usecases/index.ts
@@ -10,53 +10,32 @@ import { ConsultScenario } from "./signedInUser/consult";
 import { ObservingProjectScenario } from "./signedInUser/observingProject";
 import { ObservingUsersTasksScenario } from "./service/observingUsersTasks";
 import { ObservingUsersProjectsScenario } from "./service/observingUsersProjects";
-import { Usecase as _Usecase, AllUsecases, AllUsecasesOverDomain, UsecaseSelectorOverDomain } from "robustive-ts";
+import { AllUsecases, AllUsecasesOverDomain, Robustive, Usecase as _Usecase } from "robustive-ts";
 
-const domains = ["application", "authentication", "projectManagement"] as const;
-export type Domains = typeof domains[number];
-
-export type Requirements = {
+export const requirements = {
     application : {
-        boot : BootScenario;
+        boot : BootScenario
     }
     , authentication : {
-        signIn : SignInScenario;
-        signUp : SignUpScenario;
-        signOut : SignOutScenario;
+        signIn : SignInScenario
+        , signUp : SignUpScenario
+        , signOut : SignOutScenario
     }
     , projectManagement : {
-        /* signInUser */
-        observingProject : ObservingProjectScenario;
-        listInsuranceItems : ListInsuranceItemsScenario;
-        getWarrantyList : GetWarrantyListScenario;
-        consult : ConsultScenario;
+        observingProject : ObservingProjectScenario
+        , listInsuranceItems : ListInsuranceItemsScenario
+        , getWarrantyList : GetWarrantyListScenario
+        , consult : ConsultScenario
         /* service actor */
-        observingUsersTasks : ObservingUsersTasksScenario;
-        observingUsersProjects : ObservingUsersProjectsScenario;
+        , observingUsersTasks : ObservingUsersTasksScenario
+        , observingUsersProjects : ObservingUsersProjectsScenario
     }
 };
 
-const usecases = new UsecaseSelectorOverDomain<Requirements>();
+export const U = new Robustive(requirements);
 
-export const U = {
-    application : {
-        boot : usecases.application.boot(BootScenario)
-    }
-    , authentication : {
-        signIn : usecases.authentication.signIn(SignInScenario)
-        , signUp : usecases.authentication.signUp(SignUpScenario)
-        , signOut : usecases.authentication.signOut(SignOutScenario)
-    }
-    , projectManagement : {
-        observingProject : usecases.projectManagement.observingProject(ObservingProjectScenario)
-        , listInsuranceItems : usecases.projectManagement.listInsuranceItems(ListInsuranceItemsScenario)
-        , getWarrantyList : usecases.projectManagement.getWarrantyList(GetWarrantyListScenario)
-        , consult : usecases.projectManagement.consult(ConsultScenario)
-        /* service actor */
-        , observingUsersTasks : usecases.projectManagement.observingUsersTasks(ObservingUsersTasksScenario)
-        , observingUsersProjects : usecases.projectManagement.observingUsersProjects(ObservingUsersProjectsScenario)
-    }
-};
+export type Requirements = typeof requirements;
+type Domains = keyof Requirements;
 
 export type Usecases = AllUsecasesOverDomain<Requirements>;
 export type UsecasesOf<D extends keyof Requirements> = AllUsecases<Requirements, D>; 
@@ -66,7 +45,7 @@ export type UsecaseLog = {
     id: string;
     executing: { 
         domain: keyof Requirements;
-        usecase: { [D in keyof Requirements] : { [U in keyof Requirements[D]] : U }[ keyof Requirements[D]] }[keyof Requirements]; 
+        usecase: { [D in Domains] : keyof Requirements[D]; }[Domains]; 
     }
     startAt: Date;
 };
