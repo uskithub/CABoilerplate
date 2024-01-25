f5c803bb36f43e75fc6cbf7f72a21d54ad62c5ad
Yusuke SAITO <yusuke.saito@jibunstyle.com>
2024-01-26 06:04:13 +0900

    [wip] tree見直し

--- src/client/service/presentation/models/taskTreenode.ts
+++ src/client/service/presentation/models/taskTreenode.ts
@@ -1,44 +1,48 @@
 import type { TaskProperties } from "@/shared/service/domain/taskManagement/task";
 import { TaskStatus, TaskType } from "@/shared/service/domain/taskManagement/task";
-import type { Treenode } from "vue3-tree";
+import { findNodeById, type Treenode } from "vue3-tree";
 
 export class TaskTreenode implements Treenode<TaskProperties> {
     private _task: TaskProperties;
-    isFolding: boolean;
+    isFolding: boolean = false;
 
-    constructor(task: TaskProperties);
-    constructor(title: string, children: TaskProperties[]);
+    // constructor(task: TaskProperties);
+    // constructor(title: string, children: TaskProperties[]);
 
-    constructor(task: TaskProperties|string, children?: TaskProperties[]) {
-        if (task) {
-            this._task = task;
-        } else if (children) {
-            this._task = {
-                id: "root"
-                , type: TaskType.privateProject
-                , status: TaskStatus.open
-                , title: "my tasks"
-                , purpose: null
-                , goal: null
-                , instractions: null
-                , author: "set-current-user-id"
-                , owner: "set-current-user-id"
-                , assignees: []
-                , members: ["set-current-user-id"]
-                , involved: ["set-current-user-id"]
-                , ancestorIds: null
-                // , _children: children.map(t => t.id)
-                , children: children
-                , startedAt: null
-                , deadline: null
-                , logs: []
-                , createdAt: new Date()
-            };
-        } else {
-            throw new Error("TaskTreenode constructor must be called with either task or children.");
-        }
-        this.isFolding = false;
+    constructor(task: TaskProperties) {
+        this._task = task;
+        this.isFolding;
     }
+
+    // constructor(task: TaskProperties|string, children?: TaskProperties[]) {
+    //     if (task) {
+    //         this._task = task;
+    //     } else if (children) {
+    //         this._task = {
+    //             id: "root"
+    //             , type: TaskType.privateProject
+    //             , status: TaskStatus.open
+    //             , title: "my tasks"
+    //             , purpose: null
+    //             , goal: null
+    //             , instractions: null
+    //             , author: "set-current-user-id"
+    //             , owner: "set-current-user-id"
+    //             , assignees: []
+    //             , members: ["set-current-user-id"]
+    //             , involved: ["set-current-user-id"]
+    //             , ancestorIds: null
+    //             // , _children: children.map(t => t.id)
+    //             , children: children
+    //             , startedAt: null
+    //             , deadline: null
+    //             , logs: []
+    //             , createdAt: new Date()
+    //         };
+    //     } else {
+    //         throw new Error("TaskTreenode constructor must be called with either task or children.");
+    //     }
+    // }
 
     get id(): string { return this._task.id || ""; }
 
@@ -57,4 +61,33 @@ export class TaskTreenode implements Treenode<TaskProperties> {
     update(newTask: TaskProperties) {
         this._task = newTask;
     }
+
+    // arrange(node: TaskTreenode, from: { id: string; node: TaskTreenode; }, to: { id: string; node: TaskTreenode; }, index: number) {
+    //     const _from = findNodeById(from.id, state.donedleTree);
+    //     const _to = findNodeById(to.id, state.donedleTree);
+    //     if (_from === null || _to === null) return;
+    //     // 元親から削除
+    //     _from.content.children = _from.content.children.filter((child) => child.id !== node.id);
+    //     // 新親に追加
+    //     _to.content.children.splice(index, 0, node.content);
+    //     _to.isFolding = false;
+    // }
+    
+    toggleFolding(id: string) {
+        const node = findNodeById<TaskTreenode>(id, state.donedleTree);
+        if (node === null) return;
+        node.isFolding = !node.isFolding;
+    }
+    
+    // toggleEditing(id: string, isEditing: boolean) {
+    //     const node = findNodeById(id, state.swtTree);
+    //     if (node === null) return;
+    //     state.isEditing = isEditing;
+    // }
+    
+    // updateNode(newValue: TaskTreenode) {
+    //     const node = findNodeById(newValue.id, state.donedleTree);
+    //     if (node === null) return;
+    //     node.update(newValue.content);
+    // }
 }
--- src/client/service/presentation/views/home/MyTasks.vue
+++ src/client/service/presentation/views/home/MyTasks.vue
@@ -14,6 +14,7 @@ import { Syncable } from "@/client/system/common";
 import { inject, reactive } from "vue";
 import type { TreeEventHandlers } from "vue3-tree";
 import "vue3-tree/style.css";
+import { computed } from "vue";
 
 const { stores, dispatch } = inject<Service>(SERVICE_KEY)!;
 
@@ -28,13 +29,34 @@ const state = reactive<{
     usersTasks: new Syncable(stores.taskManagement.usersTasks)
 });
 
+const myTaskTreenodes = computed(() => {
+    return state.usersTasks.value.map(task => {
+        return new TaskTreenode(task);
+    });
+});
 
 </script>
 
 <template lang="pug">
 v-container
-  template(v-for="task in state.usersTasks.value", :key="task.id")
-    div {{ task.title }}
+  template(v-for="taskTreenode in myTaskTreenodes", :key="taskTreenode.id")
+    tree(
+      :node="taskTreenode"
+    )
+      template(v-slot="slotProps")
+        input(
+          v-if="slotProps.isEditing"
+          v-model="slotProps.node.name"
+          v-focus
+          @blur="() => { if (slotProps.endEditing) slotProps.endEditing(slotProps.node.name); }"
+        )
+        template(v-else-if="slotProps.depth===0")
+          span.header {{ slotProps.node.name }}
+          v-icon(
+            v-show="slotProps.isHovering" 
+            icon="mdi:mdi-export-variant"
+          )
+        span.title(v-else) {{ slotProps.node.name }}
   
 </template>
 
